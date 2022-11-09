e77b8564d9827237336b509bb843489f0f60e5a7
yosassenav <mromerovanessa.soy@gmail.com>
2022-11-09 01:19:38 -0500

    first attempt of implementation

--- main.js
+++ main.js
@@ -1,14 +1,42 @@
-const main =()=>{
-fetch("https://rickandmortyapi.com/api/character")
+const mainContainer = document.querySelector("#mainContainer");
 
-.then(resp=>resp.json())
-.then(data=>{
-    console.log(data)
-})
-.catch(error=>{
-    console.log(error);
-});
+const createCard = (img, name) => {
+  const cardContainer = document.createElement("div");
+  cardContainer.classList.add("card");
 
-}
-main();
+  const image = document.createElement("img");
+  image.classList.add("card-image-top");
+  image.src = img;
 
+  const div = document.createElement("div");
+  div.classList.add("card-body");
+
+  const h5 = document.createElement("h5");
+  h5.classList.add("card-title");
+  h5.innerText = name;
+
+  cardContainer.appendChild(div);
+  div.appendChild(image);
+  div.appendChild(h5);
+
+  return cardContainer;
+};
+
+const getCharacters = () => {
+  fetch("https://rickandmortyapi.com/api/character")
+    //http response in JSON format
+    .then((response) => response.json())
+    .then((data) => {
+      //store all characters in a variable
+      let characters = data.results;
+      //for each character, we retrieve the character's name and image
+      characters.forEach((character) => {
+        let images = character.image;
+        let names = character.name;
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+getCharacters();
