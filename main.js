e646633a8cb300032b1a82209cdad8b1d5311200
jessica <jessicahercon@gmail.com>
2022-11-08 21:33:20 -0600

    promise added

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
+const main =()=>{
+fetch("https://rickandmortyapi.com/api/character")
+
+.then(resp=>resp.json())
+.then(data=>{
+    console.log(data)
+})
+.catch(error=>{
+    console.log(error);
+});
+
+}
+main();
 
